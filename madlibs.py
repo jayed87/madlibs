96e22fda1af9683027ae0e709e9f5b6cfdc6ae30
jayed87 <akbarjayed87@gmail.com>
2022-06-24 22:12:58 +0200

    final code

--- madlibs.py
+++ madlibs.py
@@ -1,4 +1,14 @@
-youtuber = ""# some string variable
+# youtuber = "Jayed"# some string variable
 
-print("Subscribe to " + youtuber)
-print("i")
+# print("Subscribe to " + youtuber)
+# print("Subscribe to {}".format(youtuber))
+# print(f"Subscribe to {youtuber}")
+# print("Subscribe to %s" %youtuber)
+
+import hp, code, zombie, hungergames
+import random
+
+
+if __name__ == "__main__":
+    m = random.choice([hp, code, zombie, hungergames])
+    m.madlib()
